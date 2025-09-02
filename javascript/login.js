0c1b48a527aa72f641361a64fd4be8d11d7de2a7
BolBol-short <chhonchhaychanvibol@gmail.com>
2025-09-03 00:17:14 +0700

    add folders for js and js for login/signup form

---
+++ javascript/login.js
@@ -0,0 +1,21 @@
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
+const container = document.getElementById("form-container");
+
+loginBtn.addEventListener("click", () =>{
+    container.style.height = "21em";
+    loginBtn.classList.add("active");
+    signupBtn.classList.remove("active");
+    loginForm.classList.add("active");
+    signupForm.classList.remove("active");
+});
+
+signupBtn.addEventListener("click", () => {
+    container.style.height = "29em";
+    loginBtn.classList.remove("active");
+    signupBtn.classList.add("active");
+    loginForm.classList.remove("active");
+    signupForm.classList.add("active");
+});
